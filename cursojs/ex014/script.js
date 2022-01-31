9072db70013228050eaa2575837cc2c1eae00175
mairahardt <97862321+mairahardt@users.noreply.github.com>
2022-01-31 20:27:19 -0300

    exercicio js site sobre horarios

---
+++ cursojs/ex014/script.js
@@ -0,0 +1,23 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+var img = window.document.getElementById("imagem")
+var data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} horas.`
+if(hora >= 0 && hora < 12){
+    //BOM DIA!
+    img.src = 'imagens/manha1.png'
+    document.body.style.background = '#C1AD52'
+    
+} else if (hora >= 12 && hora < 18) {
+    //BOA TARDE!
+    img.src = 'imagens/tarde.png'
+    document.body.style.background = '#B28A89'
+} else {
+    //BOA NOITE!
+    img.src = 'imagens/noite.png'
+    document.body.style.background = '#04427B'
+}
+
+}
